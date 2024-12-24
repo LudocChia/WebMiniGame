ed1b3e2377d3573b77e3b4cf8090c71ddeab7287
ludoc <ludoc1012@outlook.com>
2024-12-24 13:36:44 +0800

    sync all

--- script/todo-list.js
+++ script/todo-list.js
@@ -1,5 +1,70 @@
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todoItems')) || [];
+document.querySelector('.todo__control-input').addEventListener('keydown', handleOneKeyDown);
+document.querySelector('.todo__button--add').addEventListener('click', () => {
+    addTodo();
+});
+
+function handleOneKeyDown(key) {
+    if (key === 'Enter') {
+        addTodo();
+    }
+}
 
 function addTodo() {
-    document.querySelector('.output__text').innerText = '';
+    const nameInputElement = document.querySelector('.todo__control-input');
+    const dateInputElement = document.querySelector('.todo__control-date');
+
+    const name = nameInputElement.value;
+    const date = dateInputElement.value;
+
+    // Simple validation: ignore empty name or date if you wish
+    if (!name || !date) return;
+
+    // Shorthand Property
+    todoList.push({ name, date });
+    localStorage.setItem('todoItems', JSON.stringify(todoList));
+    renderTodoList();
+
+    // Clear inputs after adding
+    nameInput.value = '';
+    dateInput.value = '';
 }
+
+function renderTodoList() {
+    const listElement = document.querySelector('.todo__list');
+    listElement.innerHTML = ''; // Clear to re-render
+
+    let todoItemList = '';
+
+    todoList.forEach((todo, index) => {
+        const { name, date } = todo;
+        // Destructuring
+        // const name = todoList.name;
+        // const date = todoList.date;
+        const todoItemHtml =
+            `<div class="todo__item">
+                <div class="todo__item-name">${name}</div>       
+                <div class="todo__item-date">${date}</div> 
+                <button class="todo__button todo__button--delete" class="todo__button--delete"> Delete</button>          
+            </div>`
+
+        todoItemList += todoItemHtml;
+
+        listElement.innerHTML = todoItemList;
+    });
+
+    document.querySelectorAll('.todo__button--delete')
+        .forEach((button, index) => {
+            button.addEventListener('click', () => {
+                deleteTodoItem(index);
+            });
+        });
+}
+
+function deleteTodoItem(Index) {
+    todoList.splice(Index, 1);
+    localStorage.setItem('todoItems', JSON.stringify(todoList));
+    renderTodoList();
+}
+
+renderTodoList();
